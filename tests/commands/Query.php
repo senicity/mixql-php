0be6a0d8c883f3176482fed8f88110c13c08f908
Alonzi <alonzi@Alonzis-Mac-mini.local>
2025-04-09 17:04:18 +0300

    Added support for UUID creation

--- tests/commands/Query.php
+++ tests/commands/Query.php
@@ -27,8 +27,8 @@ class Query extends CommandHelpers {
         //$result = $this->mixql->storeDelete('test-data')->execute();
         //$result = $this->mixql->storeList()->execute();
         //$result = $this->mixql->select($this->params[0])->store('test-data')->execute();
-
-        $result = $this->mixql->select('SHA1(:input, MD5(:input2))')->bind(['test1','test2'])->execute();
+        //$result = $this->mixql->select('SHA1(:input, MD5(:input2))')->bind(['test1','test2'])->execute();
+        $result = $this->mixql->createUUID()->execute();
 
         if($result == 'INVALID_INPUT'){
             echo $this->color("There is an ERROR in your query.","red","error") . "\n";
