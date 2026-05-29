f0d5b096e5f05390c0833c5366166f1b809ea4d0
Dextr <dextr@Dextrs-Mac-mini.local>
2026-05-29 03:34:13 +0300

    updates for auth and latest

--- src/Constants/Define.php
+++ src/Constants/Define.php
@@ -6,12 +6,15 @@ define('MIXQL_CREATE_KEY', 'CREATE KEY');
 define('MIXQL_CREATE_UUID', 'CREATE UUID');
 
 define('MIXQL_STORE_LIST', 'STORE LIST');
+define('MIXQL_STORE_SELECT', 'STORE SELECT ');
+define('MIXQL_STORE_USE', 'STORE USE ');
 define('MIXQL_STORE_DELETE', 'STORE DELETE ');
 
 define('MIXQL_ASHASH',' AS hash');
 define('MIXQL_LIMIT', ' LIMIT ');
 define('MIXQL_LENGTH', ' LENGTH ');
-define('MIXQL_SHA', ' SHA1');
+define('MIXQL_SHA', ' SHA');
 define('MIXQL_UPPERCASE', 'UPPERCASE');
 define('MIXQL_STOREAS', ' STORE AS ');
 define('MIXQL_STORED_QUERIES', 'STORED QUERIES');
+define('MIXQL_AUTH', 'AUTH ');
